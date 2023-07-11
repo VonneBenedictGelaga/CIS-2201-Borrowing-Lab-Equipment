1ec1ce9f7c8bab24ff961761cb8ef2dd87af67ca
Hawks <126882395+h4wks123@users.noreply.github.com>
2023-07-11 13:37:10 +0800

    Commit message for submodule changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './views/App';
+import Home from './views/Home';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
