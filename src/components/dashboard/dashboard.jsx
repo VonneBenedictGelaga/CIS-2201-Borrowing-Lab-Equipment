dadd7198e8a8ecce83826035bd1993f07a56be1b
Allen Louie Dano <akoshi777434@gmail.con>
2023-07-13 17:07:21 +0800

    Dashboard codes

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,3 +1,25 @@
 import React from 'react';
-import '../App.css'; // Assuming you have a CSS file for styling
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'src\styles\dashboardStyle.css';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <header className="dashboard-header">
+        <h1>Dashboard</h1>
+      </header>
+      <div className="dashboard-content">
+        <div className="widget">
+          <h2>Widget 1</h2>
+          {/* Add content for the first widget */}
+        </div>
+        <div className="widget">
+          <h2>Widget 2</h2>
+          {/* Add content for the second widget */}
+        </div>
+        {/* Add more widgets as needed */}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
