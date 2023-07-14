96a0ac9be7c0eafa4b4c166b53dc7d32bb6628fd
Allen Louie Dano <akoshi777434@gmail.con>
2023-07-14 18:49:33 +0800

    major changes for navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-//import { Login } from "./components/login/login.jsx"
+import { Login } from "./components/login/login.jsx"
 import Layout from  "./components/layout";
 import Navbar from "./components/navbar"
 import MainPage from "./components/main"
-import Sidebar from "./components/sidebar"
-import Dashboard from "./components/dashboard/dashboard.jsx"
+
 
 const App = () => {
   return (
     //<div className='App'><Login/></div>
     <Layout
       navbar={<Navbar />}
-      sidebar={<Sidebar />}
-      main={<Dashboard />}
+      main={<MainPage />}
+      // login={<Login />}
     />
   );
 };
