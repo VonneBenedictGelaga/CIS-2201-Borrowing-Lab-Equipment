af1a78bbc6c6fecc48d67a266adb7460120a1721
Allen Louie Dano <akoshi777434@gmail.con>
2023-07-19 22:09:38 +0800

    node modules

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-
+import 'dashboard.css';
 export const Dashboard = () => {
     return (
-      <h1>Dashboard</h1>
+      <div className='container'>
+         <div className ='1stDiv'>
+          
+         </div>
+         <div className ='2ndDiv'>
+
+         </div>
+         <div className ='3rdDiv'>
+
+         </div>
+
+      </div>
     );
   }; 
 
